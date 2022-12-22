56ef6f8c51da95a29994c0d3016686d07f1a6193
kohh <1142139793@qq.com>
2022-12-22 15:14:01 +0800

    学习了configargparse的使用

---
+++ test/test.py
@@ -0,0 +1,10 @@
+import torch
+import configargparse
+
+# parse = configargparse.ArgumentParser()
+# parse.add_argument('--config', is_config_file_arg=True, help='config path')
+# parse.add_argument('--test1', type=str, default='hello world', help='write something')
+# parse.add_argument('--test2', action='store_true', help='yes or no')
+# args = parse.parse_args()
+# print(args.test1)
+# print(args.test2)
